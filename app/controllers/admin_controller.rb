b697d66556a55d5f6465a825b5ecda43b469a8f8
MinGOODdev <mingood92@gmail.com>
2018-07-08 01:52:44 +0900

    Feat: MentorRoom Create
    멘토 권한 승인 후
    멘토방 개설

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,10 +12,28 @@ class AdminController < ApplicationController
   
   # 멘토 신청 승인 (신청자 권한 멘토로 변경)
   def mentorApplyConfirm
+    # 멘토 권한 승인
     users = User.find(params[:users])
     users.each do |u|
       u.authorization = 1
       u.save
+    end
+    
+    # 멘토방 개설
+    users.each do |u|
+      mentorApply = MentorApply.find_by(:user_id => u.id)
+      ## 해당 유저의 멘토방이 없다면 개설
+      if MentorRoom.find_by(:user_id => u.id) == nil
+        mentorRoom = MentorRoom.new
+        mentorRoom.user_id = mentorApply.user_id
+        mentorRoom.team_name = mentorApply.team_name
+        mentorRoom.subject = mentorApply.subject
+        mentorRoom.purpose = mentorApply.purpose
+        mentorRoom.content = mentorApply.content
+        mentorRoom.method = mentorApply.method
+        mentorRoom.semester_id = mentorApply.semester_id
+        mentorRoom.save
+      end
     end
   end
   
--- app/models/mentor_room.rb
+++ app/models/mentor_room.rb
@@ -1,4 +1,5 @@
 class MentorRoom < ApplicationRecord
     belongs_to :semester
+    belongs_to :user
     has_many :room_members
 end
