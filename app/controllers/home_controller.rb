727b42273043e56762cfe32936c4375ec051581a
mingooddev <mingood92@gmail.com>
2018-07-13 20:41:27 +0900

    Feat: Navbar Reformat
    navbar 재구성에 따른
    home_controller index action 수정
    
    style:
    devise/registrations/edit.html.erb
    devise/sessions/new.html.erb
    home/index.html.erb
    students/applyMentorEdit.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   
   def index
+    if current_user != nil
+      @mentorApply = MentorApply.find_by(:user_id => current_user.id)
+      @roomMember = RoomMember.find_by(:user_id => current_user.id)
+    end
   end
   
 end
