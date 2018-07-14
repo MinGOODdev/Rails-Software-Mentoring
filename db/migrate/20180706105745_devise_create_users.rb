8e258622c0abee1fe2f512c8beea7b163ee18c94
mingooddev <mingood92@gmail.com>
2018-07-14 20:35:39 +0900

    Modify: devise database
    엑셀 업로드를 대비 엑셀에 없는 데이터를 생각해
    null 가능 여부를 수정합니다.

--- db/migrate/20180706105745_devise_create_users.rb
+++ db/migrate/20180706105745_devise_create_users.rb
@@ -4,18 +4,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false   # student_number
+      t.string :encrypted_password, null: false
       
       ## 새롭게 추가한 항목들
       t.string :student_name, null: false
       t.string :grade, null: false
       t.string :phone_number, null: false
       t.string :mail, null: false
-      t.integer :department_id, null: false, default: 1
-      t.string :bank_name, null: false
-      t.string :bank_account, null: false
-      t.string :authorization, null: false, default: 2
+      t.integer :department_id, null: false
+      t.string :bank_name
+      t.string :bank_account
+      t.string :authorization, default: 2
 
       ## Recoverable
       t.string   :reset_password_token
